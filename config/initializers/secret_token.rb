17897ed010fd0a062c675c1bdf60dccf222539e9
Daniel Barlow <dan@telent.net>
2011-10-11 19:13:00 +0100

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exlibris::Application.config.secret_token = '2d4f3df4068fa0a9becb6ee18c573385660832fa2516b6784bb9dcd30a2c2c8670649a162a9af01412a14966e90d8d3a7db40675279280da55b8f58f4d8b578e'
