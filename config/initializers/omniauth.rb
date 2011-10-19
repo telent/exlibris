50cb5fafa90837bddcfdd00f76bbe939517671ba
Daniel Barlow <dan@telent.net>
2011-10-19 16:06:43 +0100

    remove warnings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 CONF=YAML::load(ERB.new(IO.read(File.join(Rails.root,'config','omniauth.yml'))).result)[Rails.env]
-warn CONF
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :developer unless Rails.env.production?
   t=CONF["twitter"]
   provider :twitter, t["consumer_key"],t["consumer_secret"]
   f=CONF["facebook"]
