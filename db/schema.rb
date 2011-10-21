f9cfc3db4488f0f9b7e66e628790f09030a85fca
Daniel Barlow <dan@telent.net>
2011-10-21 20:56:43 +0100

    images for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111019151632) do
+ActiveRecord::Schema.define(:version => 20111021193717) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(:version => 20111019151632) do
     t.string   "fullname"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
